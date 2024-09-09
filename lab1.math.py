b926c0349d578977c7046eeb2a54d1207ddf705f
ssa25 <ssa25t@mail.ru>
2024-09-09 15:20:33 +0300

    minor changes

--- lab1.math.py
+++ lab1.math.py
@@ -1,3 +1,14 @@
+import copy
+import numpy
+
+
+def pick_row(matrix: list[list], ind):
+    for x in range(ind, len(matrix)):
+        if matrix[x][ind] != 0:
+            matrix[ind][ind], matrix[x][ind] = matrix[x][ind], matrix[ind][ind]
+            return matrix, True
+    return matrix, False
+
 
 def matrix_input():
     n = int(input())
@@ -13,6 +24,7 @@ def pick_abs(row: list[float]):
         return row.index(max(row))
     return row.index(min(row))
 
+
 def vector_input():
     vector = list(map(float, input().split()))
     return vector
@@ -26,20 +38,27 @@ def merge_mat_vec(matrix: list[list], vector: list):
 
 def first_move(matrix: list[list]):
     for i in range(len(matrix)):
+        matrix, flag = pick_row(matrix, i)
+        if not flag:
+            print('Many solutions')
+            return matrix
         for j in range(i + 1, len(matrix)):
+            el_ = matrix[j][i]
             for k in range(i, len(matrix[0])):
-                matrix[j][k] -= matrix[j][i] * matrix[i][k] / matrix[i][i]
+                matrix[j][k] -= el_ * matrix[i][k] / matrix[i][i]
     return matrix
 
 
 def check_row(matrix: list[list], ind: int):
     if len(matrix[ind]) - matrix[ind].count(0) == 2:
         return True
-    elif len(matrix[ind]) - matrix[ind].count(0) == 1:
+    elif len(matrix[ind][:len(matrix[0]) - 1]) - matrix[ind].count(0) == 1:
         print('No solutions')
         return False
-    print('Many solutions')
-    return False
+    elif matrix[ind].count(0) == len(matrix[ind]):
+        print('Many solutions')
+        return False
+    return True
 
 
 def wme_move(matrix: list[list]):
@@ -50,11 +69,11 @@ def wme_move(matrix: list[list]):
             print('Many solutions')
             return matrix
         matrix[i] = list(map(lambda x: x / matrix[i][ind], matrix[i]))
-        print(matrix[i])
         for j in range(len(matrix)):
             if j != i:
+                el_ = matrix[j][ind]
                 for k in range(len(matrix[0])):
-                    matrix[j][k] -= matrix[j][ind] * matrix[i][k] / matrix[i][ind]
+                    matrix[j][k] -= el_ * matrix[i][k] / matrix[i][ind]
     return matrix
 
 
@@ -70,13 +89,15 @@ def second_move(matrix: list[list]):
     return matrix
 
 
-def gauss_solution():
-    mat = matrix_input()
-
-    vec = vector_input()
-    matrix = mat.copy()
+def gauss_solution(mat: list[list] = None, vec: list = None):
+    if not mat:
+        mat = matrix_input()
+    if not vec:
+        vec = vector_input()
+    matrix = copy.deepcopy(mat)
+    print(mat)
     mat = first_move(merge_mat_vec(mat, vec))
-
+    mat1 = copy.deepcopy(mat)
     for row in mat:
         print(*row)
 
@@ -87,7 +108,8 @@ def gauss_solution():
     solution_vector = []
     for x in mat:
         solution_vector.append(x[-1])
-    return solution_vector, matrix
+    print('Solution vector:', solution_vector)
+    return solution_vector, matrix, mat1
 
 
 def gauss_solution_wme():
@@ -95,14 +117,70 @@ def gauss_solution_wme():
     mat = matrix_input()
 
     vec = vector_input()
-    matrix = mat.copy()
+    matrix = copy.deepcopy(mat)
 
     mat = wme_move(merge_mat_vec(mat, vec))
     for row in mat:
         print(*row)
-    return mat
+    dic = dict()
+    for x in range(len(mat)):
+        for y in range(len(mat[0]) - 1):
+            if mat[x][y] != 0:
+                dic[y] = mat[x][-1]
+    solution_vector = []
+    print(dic)
+    for x in range(len(dic)):
+        solution_vector.append(dic[x])
+
+    print('Solution_vector: ', solution_vector)
+    return solution_vector, matrix, mat
 
 
-gauss_solution_wme()
+def count_nevyazka(vector: list, matrix: list[list], *args):
+    new_vector = copy.deepcopy(vector)
+    for x in range(len(matrix)):
+        for y in range(len(matrix[0])):
+            new_vector[y] -= matrix[x][y] * vector[y]
 
-gauss_solution()
+    return new_vector
+
+
+def count_e(vector: list, *args):
+    return (sum(list(map(lambda x: x**2, vector))))**(1/2)
+
+
+
+
+
+def count_det(mat: list[list] = None):
+    if not mat:
+        mat = matrix_input()
+    _, _, mat = gauss_solution(mat.copy())
+    det = 1
+    for x in range(len(mat)):
+        det *= mat[x][x]
+
+    return det
+
+
+def count_inv(mat: list[list] = None):
+    inv_mat = [[0] * len(mat) for x in range(len(mat))]
+    vector = [0] * len(mat)
+    vector[-1] = 1
+    for x in range(len(mat)):
+        vector[x] = 1
+        vector[x - 1] -= 1
+        sol_vector, _, _ = gauss_solution(copy.deepcopy(mat), vector.copy())
+        print(mat)
+        for y in range(len(sol_vector)):
+            inv_mat[y][x] = sol_vector[y]
+    return inv_mat
+
+def task1234():
+    print(vector_nev := count_nevyazka(*gauss_solution_wme()))
+    print(count_e(vector_nev))
+    print(vector_nev := count_nevyazka(*gauss_solution()))
+    print(count_e(vector_nev))
+
+task1234()
+count_inv(matrix_input())
